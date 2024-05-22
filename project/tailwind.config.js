e943cf93e4b68597a99f48eaf5f4d55ed7826481
hamza jellam <60824511+hamzajell123@users.noreply.github.com>
2024-05-22 05:50:28 +0100

    Add files via upload

---
+++ project/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'Arial', 'sans-serif'],
+        
+      },
+      fontFamily: {
+        sans: ['Golos',  'sans-serif'],
+        
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
